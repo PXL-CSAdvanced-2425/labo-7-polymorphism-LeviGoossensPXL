5c76d5887b42d418179d9afae3417e51ad5a72e5
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-04-28 09:51:54 +0200

    make general class

---
+++ Labo-7---Polymorphism/Entities/General.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_7___Polymorphism.Entities
+{
+    public class General : Machine
+    {
+        public General(string name) : base(name)
+        {
+        }
+
+        protected override int LifeSpanCostPerMinute => 1;
+
+        public override void Use(int numberOfMinutes)
+        {
+            LifeSpan -= numberOfMinutes * LifeSpanCostPerMinute;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {LifeSpanInfo()}";
+        }
+    }
+}
