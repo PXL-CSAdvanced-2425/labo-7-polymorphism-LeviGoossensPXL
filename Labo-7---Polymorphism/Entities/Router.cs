968694c33febd98fab833fd9d89c64102462fa57
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-04-28 09:23:16 +0200

    make Router class

---
+++ Labo-7---Polymorphism/Entities/Router.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_7___Polymorphism.Entities
+{
+    public class Router : Machine
+    {
+        #region Properties
+        public double WorkSpaceWidth { get; set; }
+        public double WorkSpaceLength { get; set; }
+        public double CostPerMinute { get; set; }
+        protected override int LifeSpanCostPerMinute => 50;
+        #endregion
+
+        #region Constructors
+        public Router(string name, double width, double length, double costPerMinute) : base(name)
+        {
+            WorkSpaceWidth = width;
+            WorkSpaceLength = length;
+            CostPerMinute = costPerMinute;
+
+            LifeSpan = 25000;
+        }
+        #endregion
+
+        #region Methods
+        public override void Use(int numberOfMinutes)
+        {
+            LifeSpan -= numberOfMinutes * LifeSpanCostPerMinute;
+        }
+
+        public override string ToString()
+        {
+            return $"ROUTER: '{Name}', ({WorkSpaceWidth}x{WorkSpaceLength}), <{LifeSpanInfo()}>";
+        }
+        #endregion
+    }
+}
