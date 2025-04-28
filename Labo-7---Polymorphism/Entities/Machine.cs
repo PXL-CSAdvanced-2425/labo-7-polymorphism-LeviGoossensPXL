8c1b76ba6623ba5c23df57131eb3b39369d85ce1
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-04-28 09:05:11 +0200

    make Machine Class

---
+++ Labo-7---Polymorphism/Entities/Machine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_7___Polymorphism.Entities
+{
+    public abstract class Machine
+    {
+        #region Properties
+        public string Name { get; set; }
+        public int LifeSpan { get; set; }
+        public float Price { get; set; }
+        public bool OutOfUse => LifeSpan <= 0;
+
+        protected abstract int LifeSpanCostPerMinute { get; }
+        #endregion
+
+        #region Constructors
+        protected Machine(string name)
+        {
+            Name = name;
+        }
+        #endregion
+
+        #region Methods
+        public abstract void Use(int numberOfMinutes);
+
+        public string LifeSpanInfo()
+        {
+            if (OutOfUse)
+            { 
+                return "OUT OF USE";
+            }
+            return $"<Lifespan: {LifeSpan} h>";
+        }
+
+        public override string ToString()
+        {
+            return LifeSpanInfo();
+        }
+        #endregion
+    }
+}
